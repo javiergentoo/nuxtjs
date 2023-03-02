12e5217fabbd00a5ebefe9c35e6ce0fe6e9c6aa5
Javier Martin <javier.gentoo@gmail.com>
2023-03-02 09:15:16 +0100

    cambio

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">nuxtjs</h1>
+      <h1 class="title">nuxtjs desde Javier Martin</h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
